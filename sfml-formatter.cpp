127b67ff874daefa27f0d1dac0abdd7a1b047ac3
Reshetov Ivan <re@mail.ru>
2025-07-14 21:17:29 +0300

    добавлена клавиатура

--- sfml-formatter.cpp
+++ sfml-formatter.cpp
@@ -1,5 +1,7 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+#include "windows.h"
 using namespace std;
 
 int main()
@@ -8,6 +10,26 @@ int main()
     sf::Font font;
     sf::Text header;
     sf::Text subtitle;
+    
+    std::vector<sf::RectangleShape> btns;
+    std::vector<sf::Text> btnsText;
+
+    const sf::Vector2f SIZE_BTN(50.f, 30.f);
+
+    for (int i = 0; i < 10; i++) {
+        sf::RectangleShape btn(SIZE_BTN);
+        sf::Text text;
+        text.setFont(font);
+        text.setCharacterSize(18);
+        text.setString(std::to_string(i));
+        text.setFillColor(sf::Color::Black);
+
+        btn.setPosition(sf::Vector2f(i*60, 100));
+        btns.push_back(btn);
+        sf::Vector2f position = btn.getPosition();
+        text.setPosition(sf::Vector2f(position.x+15, position.y+6));
+        btnsText.push_back(text);
+    }
 
     font.loadFromFile("./fonts/arialbd.ttf");
 
@@ -26,11 +48,38 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    auto mousePosition = event.mouseButton;
+                    for (int i = 0; i < btns.size(); i++){
+                        if (mousePosition.x >= btns[i].getPosition().x &&
+                            mousePosition.x <= btns[i].getPosition().x + SIZE_BTN.x &&
+                            mousePosition.y >= btns[i].getPosition().y &&
+                            mousePosition.y <= btns[i].getPosition().y + SIZE_BTN.y) {
+                            btns[i].setFillColor(sf::Color::Green);
+                            std::cout << btnsText[i].getString().toAnsiString();
+                        }
+                    } 
+                }
+            }
+            else {
+                for (int i = 0; i < btns.size(); i++) {
+                    btns[i].setFillColor(sf::Color::White);
+                }
+            }
         }
 
         window.clear();
         window.draw(header);
         window.draw(subtitle);
+        for (auto btn : btns) {
+            window.draw(btn);
+        }
+        for (auto text : btnsText) {
+            window.draw(text);
+        }
 
         window.display();
     }
