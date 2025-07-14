3f43be431bd59a14c114f53c1515aa81523cc8cb
Reshetov Ivan <re@mail.ru>
2025-07-14 19:05:18 +0300

    создан базовый проект sfml

---
+++ sfml-formatter.cpp
@@ -0,0 +1,25 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::CircleShape circle(20.f);
+    circle.setFillColor(sf::Color::Cyan);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(circle);
+
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
